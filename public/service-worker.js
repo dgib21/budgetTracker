5eb4957a71d1489cc9e847d6a54ab0d39ff57596
Devin Gibson <dgib21@hotmail.com>
2020-12-14 10:45:59 -0600

    fixing service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -25,7 +25,7 @@ self.addEventListener('install', function (e) {
         return cache.addAll(FILES_TO_CACHE)
       })
     )
-  })
+  });
   
   // delete the old caches 
   self.addEventListener('activate', function(e) {
